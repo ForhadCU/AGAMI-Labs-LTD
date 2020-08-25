3534d5537df2f86c027f061f399f8bce6585ed66
ForhadCU <kashem.tipo@gmail.com>
2020-08-25 19:20:22 +0600

    After moderating Splash Screen

--- School App/app/src/main/java/com/example/schoolapp/SplashScreenActivity.java
+++ School App/app/src/main/java/com/example/schoolapp/SplashScreenActivity.java
@@ -6,6 +6,7 @@ import android.content.Intent;
 import android.content.SharedPreferences;
 import android.os.Bundle;
 import android.os.Handler;
+import android.os.Looper;
 import android.preference.PreferenceManager;
 import android.util.Log;
 import android.view.View;
@@ -27,7 +28,7 @@ import com.android.volley.toolbox.StringRequest;
 
 import java.util.Objects;
 
-public class SplashScreenActivity extends AppCompatActivity {
+public class SplashScreenActivity extends AppCompatActivity implements Response.ErrorListener, Response.Listener<String> {
     private static final String TAG = "My Tracing : ";
     String strRes;
 //    private RequestQueue requestQueue;
@@ -51,11 +52,46 @@ public class SplashScreenActivity extends AppCompatActivity {
         progressBar = findViewById(R.id.launcherProgressBarId);
         textViewRetry = findViewById(R.id.txV_retry);
 
-      /*  Cache cache = new DiskBasedCache(getCacheDir(), 1024*1024);
+        textViewRetry.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                textViewRetry.setVisibility(View.GONE);
+                mGetResponse();
+
+            }
+        });
+
+       /* Cache cache = new DiskBasedCache(getCacheDir(), 1024*1024);
         Network network = new BasicNetwork(new HurlStack());
         requestQueue = new RequestQueue(cache, network);
         requestQueue.start();*/
 
+       //1. User er data ache kina check kora, jodi thake taile hander er code run e third Activity te jaoar code kore dite hove.
+        //2. jodi na thake,, taile getResponse call
+
+        boolean hasUserData = false;
+
+        if (hasUserData)
+        {
+            new Handler(Looper.getMainLooper()).postDelayed(new Runnable() {
+                @Override
+                public void run() {
+                    Log.e(TAG, "run: into the Handler ");
+                    mIntent(MainActivity.class);
+                }
+            }, 5000);
+        }else {
+            mGetResponse();
+        }
+
+
+
+
+//        Toast.makeText(this, "After initializing handler", Toast.LENGTH_SHORT).show();
+        Log.d(TAG, "onCreate: After initializing handler");
+
+
+/*
         Thread thread = new Thread(new Runnable() {
             @Override
             public void run() {
@@ -92,6 +128,7 @@ public class SplashScreenActivity extends AppCompatActivity {
 
                 }
 
+*/
 /*                else if (!mGetResponse())
                 {
                     runOnUiThread(new Runnable() {
@@ -111,87 +148,71 @@ public class SplashScreenActivity extends AppCompatActivity {
                         }
                     });
 
-                }*/
+                }*//*
+
             }
         });
-        thread.start();
+*/
+//        thread.start();
 
     }
 
-    private void mIntent() {
-        Intent intent = new Intent(SplashScreenActivity.this, MainActivity.class);
+
+    private void mIntent(Class target) {
+        Intent intent = new Intent(SplashScreenActivity.this, target);
         intent.setFlags(Intent.FLAG_ACTIVITY_CLEAR_TOP);
         startActivity(intent);
         finish();
     }
-    void mThreadSleep(){
+
+/*    void mThreadSleep(){
         try {
             Thread.sleep(2000);
 //            mIntent();
         } catch (InterruptedException e) {
             e.printStackTrace();
         }
-    }
+    }*/
 
 
     private boolean mGetResponse() {
-            StringRequest stringRequest = new StringRequest(Request.Method.GET, myUrl,
-                    new Response.Listener<String>() {
-                        @Override
-                        public void onResponse(String response) {
-                            int count = 0;
-                            SharedPreferences sharedPreferences = PreferenceManager.getDefaultSharedPreferences(getApplicationContext());
-                            SharedPreferences.Editor editor = sharedPreferences.edit();
-                            editor.putString("count", ""+response.substring(0, 500));
-
-                            for (int i =0 ; i<100000; i++)
-                            {
-                                count = count+i;
-                            }
-                            editor.putInt("number", count);
-                            editor.apply();
-                            requestSuccess = true;
-                            Log.d(TAG, "onResponse: called");
-//                            requestQueue.stop();
-
-                        }
-                    }, new Response.ErrorListener() {
-                @Override
-                public void onErrorResponse(VolleyError error) {
-                    Toast.makeText(SplashScreenActivity.this, error.toString(), Toast.LENGTH_SHORT).show();
-//                    requestQueue.stop();
-                    requestSuccess = false;
-                    progressBar.setVisibility(View.GONE);
-                    textViewRetry.setVisibility(View.VISIBLE);
-                 /*   progressBar.setVisibility(View.GONE);
-                    textViewRetry.setVisibility(View.VISIBLE);
-                    textViewRetry.setOnClickListener(new View.OnClickListener() {
-                        @Override
-                        public void onClick(View v) {
-                            textViewRetry.setVisibility(View.GONE);
-                            progressBar.setVisibility(View.VISIBLE);
-                            mGetResponse();
-
-                            if (requestSuccess)
-                                mIntent();
-                            else
-                            {
-                                textViewRetry.setVisibility(View.VISIBLE);
-                                progressBar.setVisibility(View.GONE);
-                            }
-
-                        }
-                    });*/
-
-                }
-            });
+            StringRequest stringRequest = new StringRequest(Request.Method.GET, myUrl, this, this);
+            progressBar.setVisibility(View.VISIBLE);
 //            requestQueue.add(stringRequest);
         MySingletonModel.getInstance(SplashScreenActivity.this).addToRequestQueue(stringRequest);
         Log.d(TAG, "mGetResponse: requestSuccess return");
             return requestSuccess;
     }
 
+    @Override
+    public void onErrorResponse(VolleyError error) {
+        textViewRetry.setVisibility(View.VISIBLE);
+        progressBar.setVisibility(View.GONE);
+    }
 
+    @Override
+    public void onResponse(String response) {
+
+
+            //Process goes here
+//        mIntent(Registration.class);
+        progressBar.setVisibility(View.GONE);
+    }
+
+/*    private Response.Listener successListener = new Response.Listener<String>() {
+        @Override
+        public void onResponse(String response) {
+
+
+        }
+    };
+
+    private Response.ErrorListener errorListener = new Response.ErrorListener() {
+        @Override
+        public void onErrorResponse(VolleyError error) {
+
+        }
+    };*/
 
     /*    private void mProgressBarLoad() {
         for (progress = 0 ; progress<100; progress++)
